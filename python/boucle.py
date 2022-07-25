c3a4e9774b54d35939eaf654a833091d31315ab6
cquentin26200 <qcheronpedenschool.fr>
2022-07-26 00:24:21 +0200

    add double boucle

--- python/boucle.py
+++ python/boucle.py
@@ -1,10 +1,10 @@
-a = [i for i in range(100)]
+a = [i for i in range(1)]
 
 print(a)
 
 b = []
 
-for i in range(100) :
+for i in range(1) :
     b.append(i)
 
 print(b)
@@ -12,3 +12,12 @@ print(b)
 c = []
 d = 0
 
+array1 = ["toto", "tata", 1, 90]
+array2 = [90, "toto", 4, 8]
+array3 = []
+
+for i in array1 :
+    for o in array2 :
+        if i == o :
+            array3.append(i)
+print(array3)
