15e0d51809f37ca40b22afd7d63143c269cf744d
cquentin26200 <qcheronpedenschool.fr>
2022-07-26 01:42:54 +0200

    add class for setter and getter

--- python/class.py
+++ python/class.py
@@ -7,7 +7,8 @@ class Person() :
     
     def getIMC(self) :
         return self.weight / (self.height * self.height)
-
+    
+#Crée une variable qui sera égal à la class pour pouvoir le lui donnée les valeurs définit dans le init
 moi = Person("noir", "noir", 65, 1.85)
 
 moi.getIMC()
@@ -29,3 +30,32 @@ Mars.get_description()
 Mars.distance_of_sun = 228
 
 Mars.get_description()
+
+class Animal() :
+    def __init__(self, sexe, race, color) :
+        self.sexe = sexe
+        self.__race = race
+        self.color = color
+        
+    #getter permet de récupérer le race
+        
+    def get_race(self) :
+        return self.__race
+    
+    #setter permet de donnée une nouvelle valeur sur le race
+    
+    def set_race1(self, new_race) :
+        self.__race = new_race
+    
+    def set_race2(self, new_race) :
+        self.__race = new_race
+
+lion = Animal("mal", "lion", "white")
+
+lion.set_race1("Lion de la savanne")
+
+print(lion.get_race())
+
+lion.set_race2("Lion de la jungle")
+
+print(lion.get_race())
